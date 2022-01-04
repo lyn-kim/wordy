0570b2df66b6af0457ae8e91020442c785bbfc1c
Lyn Kim <lynkim216@outlook.com>
2022-01-03 18:12:36 -0800

    create XMLHttpRequest object to retrieve random word from api

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
-var clickHereButton = document.querySelector('click-here-btn');
+var clickHereButton = document.querySelector('.click-here-btn');
 clickHereButton.addEventListener('click', generateWord);
 function generateWord(event) {
-
-  goToNewWord();
+  getNewWord();
+  // goToNewWord();
 }
 
-function goToNewWord() {
-  switchView('new-word');
-}
+// function goToNewWord() {
+//   switchView('new-word');
+// }
 
-var $views = document.querySelectorAll('div[data-view]');
+// var $views = document.querySelectorAll('div[data-view]');
 
-function switchView(dataView) {
-  for (var i = 0; i < $views.length; i++) {
-    if ($views[i].getAttribute('data-view') === dataView) {
-      $views[i].className = 'row';
-    } else {
-      $views[i].className = 'row hidden';
-    }
-  }
+// function switchView(dataView) {
+//   for (var i = 0; i < $views.length; i++) {
+//     if ($views[i].getAttribute('data-view') === dataView) {
+//       $views[i].className = 'row';
+//     } else {
+//       $views[i].className = 'row hidden';
+//     }
+//   }
+// }
+
+function getNewWord() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://random-words-api.vercel.app/word');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log(xhr.response);
+  });
+  xhr.send();
 }
