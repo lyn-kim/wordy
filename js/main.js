46e660b37cabd210166a2f4a095f230076dd54f7
Lyn Kim <lynkim216@outlook.com>
2022-01-04 19:07:35 -0800

    create card view with save button

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@ var clickHereButton = document.querySelector('.click-here-btn');
 clickHereButton.addEventListener('click', generateWord);
 function generateWord(event) {
   getNewWord();
+  goToNewWord();
+}
+
+function goToNewWord() {
+  switchView('new-word');
+}
+
+var $views = document.querySelectorAll('div[data-view]');
+
+function switchView(dataView) {
+  for (var i = 0; i < $views.length; i++) {
+    if ($views[i].getAttribute('data-view') === dataView) {
+      $views[i].className = 'row';
+    } else {
+      $views[i].className = 'row hidden';
+    }
+  }
 }
 
 function getNewWord() {
